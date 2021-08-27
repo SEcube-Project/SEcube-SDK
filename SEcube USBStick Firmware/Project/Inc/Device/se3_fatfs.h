7ff675f0219aa42046a7dd960706e1852ef9c87b
stokdam <s269745@studenti.polito.it>
2021-08-27 17:17:16 +0200

    sefile structures

--- SEcube USBStick Firmware/Project/Inc/Device/se3_fatfs.h
+++ SEcube USBStick Firmware/Project/Inc/Device/se3_fatfs.h
@@ -51,7 +51,7 @@ typedef enum {
 	SE3_FR_FILENAME_ENC_ERROR
 } SE3_FRESULT;
 
-SE3_FRESULT secure_open(SE3_FIL* se_fp, char *path, int32_t mode, int32_t creation, uint32_t keyID, uint16_t algo);
+SE3_FRESULT secure_open(SE3_FIL* se_fp, char *path, BYTE mode, uint32_t keyID, uint16_t algo);
 SE3_FRESULT secure_close(SE3_FIL* fp);
 SE3_FRESULT secure_read(SE3_FIL* fp, uint8_t *dataOut, uint32_t dataOut_len, uint32_t *bytesRead);
 SE3_FRESULT secure_write(SE3_FIL* fp, uint8_t *dataIn, uint32_t dataIn_len);
--- SEcube USBStick Firmware/Project/Src/main.c
+++ SEcube USBStick Firmware/Project/Src/main.c
@@ -154,7 +154,7 @@ int main(void)
 
 
 	SE3_FIL fp;
-	res = secure_open(&fp, "prova2.txt", FA_CREATE_ALWAYS | FA_WRITE, 0, 1, 0);
+	res = secure_open(&fp, "prova2.txt", FA_CREATE_ALWAYS | FA_WRITE, 1, 0);
 	UINT bw;
 
 	char buff[5] = "eja2";
