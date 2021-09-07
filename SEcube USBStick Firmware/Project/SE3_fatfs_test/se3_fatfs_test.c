e0e30ec5b5ac19f115a4ecd13af32e3b4997c9cc
stokdam <damiano.stochino@gmail.com>
2021-09-07 15:59:32 +0200

    Update se3_fatfs_test.c

--- SEcube USBStick Firmware/Project/SE3_fatfs_test/se3_fatfs_test.c
+++ SEcube USBStick Firmware/Project/SE3_fatfs_test/se3_fatfs_test.c
@@ -12,6 +12,7 @@ int test_seek();
 int test_invalid_file();
 int test_unopen_file();
 int test_negative_seek();
+int test_write_read_dir();
 
 uint8_t payload[PAYLOAD_SIZE] =
 				"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu metus augue. Maecenas justo felis, mollis et erat a, viverra ullamcorper quam. Ut urna felis, tempus imperdiet ornare auctor, iaculis non sapien. Integer luctus, erat in hendrerit imperdiet, augue tellus placerat orci, ut egestas sapien ipsum non massa. Pellentesque ut laoreet erat, sit amet sagittis nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi sit amet nisl tincidunt ex porta sollicitudin eu vitae turpis. Vestibulum ut blandit dui. Nunc eu diam maximus, imperdiet nulla sodales, ultrices enim.Mauris ornare eros elementum arcu viverra hendrerit. Vestibulum urna ligula, auctor a dapibus id, tincidunt ut nunc. Aliquam non dolor ligula. Mauris maximus lorem diam, vitae dignissim ante lobortis a. Quisque sollicitudin lorem vitae sapien tincidunt, quis luctus lorem tincidunt. Integer dolor enim, viverra vitae pharetra sit amet, porttitor eu urna. Nullam vel laoreet ipsum, nec fringilla orci. Phasellus convallis malesuada nulla, quis sagittis sapien tincidunt a. Nunc at sagittis dui, sed blandit ante.Donec interdum nisi et pellentesque lacinia. Vestibulum ultricies ultrices erat in luctus. Maecenas nec turpis sit amet ex posuere feugiat. Donec sed bibendum nulla. Ut eu nunc bibendum, interdum massa tempus, ornare lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer augue magna, pulvinar vel metus sed, blandit finibus elit.Integer tincidunt neque in blandit cursus. Donec eu vestibulum dolor. Etiam consectetur eros nulla, faucibus malesuada libero aliquam ut. Nam est ligula, porta sit amet porttitor vitae, varius non neque. Duis gravida sed orci at pellentesque. Donec lectus ligula, pellentesque nec erat rhoncus, cursus tincidunt massa. Etiam commodo luctus quam, auctor vulputate massa sodales in. Vestibulum ac suscipit sapien, a viverra mi.Nam eu metus magna. Duis mollis turpis ante. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent eget sem a odio dapibus mattis vitae a nisl. Donec a lorem ante. Aliquam quis accumsan neque, id vulputate est. Morbi sit amet ante aliquet, viverra lacus et, posuere nisl. Suspendisse eu mauris tellus.Suspendisse sed quam nec nulla blandit molestie sit amet in ligula. Pellentesque venenatis dapibus lectus. ";
@@ -51,6 +52,7 @@ int begin_test()
 	tests_failed += execute_test(test_invalid_file, "test_invalid_file");
 	tests_failed += execute_test(test_unopen_file, "test_unopen_file");
 	tests_failed += execute_test(test_negative_seek, "test_negative_seek");
+	tests_failed += execute_test(test_write_read_dir, "test_write_read_dir");
 
 	f_close(&test_report);
 	return tests_failed;
@@ -295,6 +297,35 @@ int test_negative_seek()
 	if(secure_seek(&se_fp, -40000, &position, SE3_FATFS_END) != SE3_FR_SEEK_ERROR)
 		return TEST_FAIL;
 	secure_close(&se_fp);
+
+	return TEST_PASS;
+}
+
+int test_write_read_dir()
+{
+	uint8_t buffOut[3000];
+	memset(buffOut, 1, 3000);
+	SE3_FIL fp;
+	SE3_FRESULT res;
+	uint32_t bytes_read;
+
+	res = 0;
+
+	f_mkdir("dir");
+
+	res += secure_open(&fp, "dir/prova3.txt", FA_CREATE_ALWAYS | FA_WRITE, 1, SE3_ALGO_AES_HMACSHA256);
+	res += secure_write(&fp, (void *) payload, PAYLOAD_SIZE);
+	res += secure_close(&fp);
+
+	res += secure_open(&fp, "dir/prova3.txt", FA_READ, 1, SE3_ALGO_AES_HMACSHA256);
+	res += secure_read(&fp, buffOut, PAYLOAD_SIZE, &bytes_read);
+	res += secure_close(&fp);
+
+	if (res)
+		return TEST_FAIL;
+
+	if (memcmp(payload, buffOut, PAYLOAD_SIZE))
+		return TEST_FAIL;
 
 	return TEST_PASS;
 }
